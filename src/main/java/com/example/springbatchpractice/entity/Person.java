bf8426bde55884d580ac92647d469f23f423cf34
Nickolodeon98 <sjeon0730@gmail.com>
2023-02-22 10:17:55 +0900

    build: add configured table name to Person entity to change actual table name in DB

--- src/main/java/com/example/springbatchpractice/entity/Person.java
+++ src/main/java/com/example/springbatchpractice/entity/Person.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +18,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @Setter
 @Builder
 @Entity
+@Table(name = "people")
 public class Person {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
