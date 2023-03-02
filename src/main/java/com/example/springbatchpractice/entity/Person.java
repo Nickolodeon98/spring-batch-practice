8b30ffed156b5651642b81c5cbc8178bdd4d1e10
Nickolodeon98 <sjeon0730@gmail.com>
2023-03-03 05:44:45 +0900

    feat: change domain class Person that represents a data table not to use JPA

--- src/main/java/com/example/springbatchpractice/entity/Person.java
+++ src/main/java/com/example/springbatchpractice/entity/Person.java
@@ -1,35 +1,19 @@
 package com.example.springbatchpractice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Builder
-@Entity
-@Table(name = "people")
 public class Person {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long personId;
   private String lastName;
   private String firstName;
-
-  public Person(String lastName, String firstName) {
-    this.lastName = lastName;
-    this.firstName = firstName;
-  }
 
   @Override
   public String toString() {
